b4e7d3efb71cf010874dd91aed35a71caacfd6af
Vinicio <39930872+viniciosix@users.noreply.github.com>
2026-09-18 02:29:47 -0400

    Create dental clinic demo

---
+++ script.js
@@ -0,0 +1,1 @@
+const menu=document.querySelector('.menu'),nav=document.querySelector('nav');menu.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
